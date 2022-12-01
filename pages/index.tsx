0665009db33dfc71040719b98324099462501431
Qreamville <Oyeleyebalikis@yahoo.com>
2022-12-01 02:33:29 -1200

    initialize project using create next app

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Head from '../components/common/Head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+     <Head />
+    </div>
+  );
+};
+
+export default Home;
